884d6a5aace405e9f802bdcd3bd1406f1bed52d7
Gabin L <gabin.lanore@gmail.com>
2025-04-25 00:17:45 +0200

    feat: add windows images
    Signed-off-by: Gabin L <gabin.lanore@gmail.com>

--- windows.tf
+++ windows.tf
@@ -1,20 +1,73 @@
-resource "openstack_images_image_v2" "windows_10_pro" {
-  name             = "Windows 10 Pro"
-  local_file_path  = "local_files/windows-10-pro.qcow2"
+resource "openstack_images_image_v2" "windows_server_2016_standard_core" {
+  name             = "Windows Server 2016 Standard Core"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2016_standard_core.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
   visibility       = "public"
   protected        = false
-  min_disk_gb      = "20"
-  min_ram_mb       = "2048"
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
 
   properties = {
     hw_scsi_model       = "virtio-scsi"
     hw_disk_bus         = "scsi"
     hw_qemu_guest_agent = "yes"
     os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
     os_type             = "windows"
-    os_distro           = "windows-10"
+    os_distro           = "windows"
+    os_version          = "2016_standard_core"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_server_2016_standard" {
+  name             = "Windows Server 2016 Standard"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2016_standard.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "2016_standard"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_server_2019_standard_core" {
+  name             = "Windows Server 2019 Standard Core"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2019_standard_core.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "2019_standard_core"
     hw_rescue_bus       = "scsi"
     hw_rescue_device    = "disk"
   }
@@ -22,21 +75,124 @@ resource "openstack_images_image_v2" "windows_10_pro" {
 
 resource "openstack_images_image_v2" "windows_server_2019_standard" {
   name             = "Windows Server 2019 Standard"
-  local_file_path  = "local_files/windows-server-2019-standard.qcow2"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2019_standard.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
   visibility       = "public"
   protected        = false
-  min_disk_gb      = "20"
-  min_ram_mb       = "2048"
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
 
   properties = {
     hw_scsi_model       = "virtio-scsi"
     hw_disk_bus         = "scsi"
     hw_qemu_guest_agent = "yes"
     os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
     os_type             = "windows"
-    os_distro           = "windows-server"
+    os_distro           = "windows"
+    os_version          = "2019_standard"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_server_2022_standard_core" {
+  name             = "Windows Server 2022 Standard Core"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2022_standard_core.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "2022_standard_core"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_server_2022_standard" {
+  name             = "Windows Server 2022 Standard"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_server_2022_standard.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "2022_standard"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_10_pro" {
+  name             = "Windows 11 Pro"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_10_pro.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "10_pro"
+    hw_rescue_bus       = "scsi"
+    hw_rescue_device    = "disk"
+  }
+}
+
+resource "openstack_images_image_v2" "windows_11_pro" {
+  name             = "Windows 11 Pro"
+  image_source_url = "https://s3.netbytes.space/cloud-images/windows_11_pro.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+  protected        = false
+  min_disk_gb      = 25
+  min_ram_mb       = 4086
+  image_cache_path = var.images_cache
+
+  properties = {
+    hw_scsi_model       = "virtio-scsi"
+    hw_disk_bus         = "scsi"
+    hw_qemu_guest_agent = "yes"
+    os_require_quiesce  = "yes"
+    os_admin_user       = "admin"
+    os_type             = "windows"
+    os_distro           = "windows"
+    os_version          = "11_pro"
     hw_rescue_bus       = "scsi"
     hw_rescue_device    = "disk"
   }
